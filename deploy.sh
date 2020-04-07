0f7db2664db9b09c5012d77728fb5e5b0eea0e62
Mike Cohen <mjoecohen@gmail.com>
2020-04-07 09:41:47 +0100

    docker build process

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,13 @@ fi
 echo -----------------------------------------------------------------------------------;
 echo building all back ends;
 echo -----------------------------------------------------------------------------------;
-#mvn -f ../ms-assets/pom.xml -Dmaven.test.skip=true clean install
-#mvn -f ../ms-clients/pom.xml -Dmaven.test.skip=true clean install
-#mvn -f ../ms-mesh/pom.xml -Dmaven.test.skip=true clean install
+mvn -f ../ms-assets/pom.xml -Dmaven.test.skip=true clean install
+mvn -f ../ms-clients/pom.xml -Dmaven.test.skip=true clean install
+mvn -f ../ms-mesh/pom.xml -Dmaven.test.skip=true clean install
 echo -----------------------------------------------------------------------------------;
 echo building all front ends;
 echo -----------------------------------------------------------------------------------;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 pushd ../fe-shellapp;
 #./deploy.sh;
 popd;
@@ -33,26 +34,31 @@ printf "\n----------------------------------------------------------------------
 DOCKER_ID_USER='mijoco'
 DOCKER_COMPOSE_CMD='docker-compose'
 DOCKER_CMD='docker'
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 $DOCKER_COMPOSE_CMD build
-$DOCKER_COMPOSE_CMD push
+pwd
+#$DOCKER_COMPOSE_CMD -f docker-compose.yml push
+#exit 0;
 #--force-rm --no-cache
 
 #$DOCKER_CMD login -u mijoco --password-stdin
-
+#sleep 10
 printf "\n- Tagging ---------------------------------------------------------------------------------------------------\n";
-#$DOCKER_CMD tag rs-docker_mesh  $DOCKER_ID_USER/rs-docker_mesh
-#$DOCKER_CMD push $DOCKER_ID_USER/rs-docker_mesh
 
-#$DOCKER_CMD tag rs-docker_nginx  $DOCKER_ID_USER/rs-docker_nginx
-#$DOCKER_CMD push $DOCKER_ID_USER/rs-docker_nginx
+$DOCKER_CMD tag rs-docker_clients  $DOCKER_ID_USER/rs-docker_clients
+$DOCKER_CMD push $DOCKER_ID_USER/rs-docker_clients
 
-#$DOCKER_CMD tag rs-docker_assets  $DOCKER_ID_USER/rs-docker_assets
-#$DOCKER_CMD push $DOCKER_ID_USER/rs-docker_assets
+$DOCKER_CMD tag rs-docker_mesh  $DOCKER_ID_USER/rs-docker_mesh
+$DOCKER_COMPOSE_CMD push mesh
 
-#$DOCKER_CMD tag rs-docker_clients  $DOCKER_ID_USER/rs-docker_clients
-#$DOCKER_CMD push $DOCKER_ID_USER/rs-docker_clients
+$DOCKER_CMD tag rs-docker_nginx  $DOCKER_ID_USER/rs-docker_nginx
+$DOCKER_COMPOSE_CMD push nginx
+
+$DOCKER_CMD tag rs-docker_assets  $DOCKER_ID_USER/rs-docker_assets
+$DOCKER_COMPOSE_CMD push assets
+
+
+exit 0;
 
 printf "\n-----------------------------------------------------------------------------------------------------\n";
 printf "Moving to production server to pull new images....\n"
@@ -62,8 +68,9 @@ ssh -i ~/.ssh/id_rsa -p 7019 bob@$SERVER "
   cd ~/hubgit/rs-docker
   git pull
 	cp .env-prod .env
-  docker-compose pull
-  docker-compose up -d
+	docker login
+  docker-compose -f docker-compose-images.yml pull
+  docker-compose -f docker-compose-images.yml up -d
 ";
 
 popd > /dev/null
